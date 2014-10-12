e2327707e2bde43ff0e85667a717081a323ca468
Simeon Nedkov <simeon@ndkv.nl>
2014-10-13 00:35:48 +0200

    create minified environment

--- gruntfile.js
+++ gruntfile.js
@@ -1,22 +1,38 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		// concat: {
-		// 	options: {
-		// 		separator: ";"
-		// 	},
-		// 	dist: {
-		// 		src: ['js/*.js'],
-		// 		dest: 'dist/<%= pkg.name %>.js'
-		// 	}
-		// },
+		concat: {
+			// options: {
+			// 	separator: ";"
+			// },
+			css: {
+				src: ['css/*.css', '!css/style.css'],
+				dest: 'dist/css/vendor.css'
+			}
+		},
+		copy: {
+			css: {
+				src: ['css/style.css'],
+				dest: 'dist/css/style.css'
+			}
+		},
 		uglify: {
 			options: {
 			},
-			dist: {
+			dest: {
 				files: {
-					'dist/js/index.min.js': ['dist/js/index.js']
+					'dist/js/index.js': ['dist/js/index.js']
 				}
+			}
+		},
+		cssmin: {
+			vendor: {
+				src: 'dist/css/vendor.css',
+				dest: 'dist/css/vendor.css'
+			},
+			style: {
+				src: 'dist/css/style.css',
+				dest: 'dist/css/style.css'	
 			}
 		},
 		jshint: {
@@ -49,15 +65,12 @@ module.exports = function(grunt) {
 				// tasks: ['jshint', 'concat', 'browserify']
 				tasks: ['jshint']
 				// tasks: ['browserify']
-
 			},
 			css: {
-				files: ['css/*.css']
+				files: ['css/*.css'],
 				// tasks: ['jshint', 'browserify', 'concat', 'uglify']
-				// tasks: ['jshint', 'concat']
-
+				 tasks: ['copy']
 			}
-
 		}
 	});
 
@@ -65,12 +78,15 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	// grunt.loadNpmTasks('grunt-contrib-qunit');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	// grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-browserify');
+	grunt.loadNpmTasks('grunt-css');
 	grunt.registerTask('test', ['jshint']);
 
+
 	//grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'watch']);
-	grunt.registerTask('default', ['browserify', 'watch']);
-	grunt.registerTask('minify', ['uglify']);
+	grunt.registerTask('default', ['browserify', 'concat', 'copy', 'watch']);
+	grunt.registerTask('minify', ['uglify', 'concat', 'copy', 'cssmin']);
 
 };
