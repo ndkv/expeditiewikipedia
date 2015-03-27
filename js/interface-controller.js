4a381cc14b9768e9210ce4502477e1856ec2be76
Simeon Nedkov <simeon@ndkv.nl>
2015-03-27 17:46:28 +0100

    add English texts to intropane

--- js/interface-controller.js
+++ js/interface-controller.js
@@ -176,8 +176,7 @@ var InterfaceController = function(ExpeditionController) {
 		});
 
 		//change language
-		$('.spacer-left-title').html('kies een reis');
-		$('.spacer-left-summary').html('stap in en doe mee met de interessante expedities en ontdek alles over geschiedenis en techniek en leer wat wetenschap zo rijk maakt!');
+
 
 		buildSwiper();
 	};
@@ -606,6 +605,9 @@ var InterfaceController = function(ExpeditionController) {
 				value.firstChild.innerHTML = 'Read more';
 			});
 
+			$('.spacer-left-title').html('choose an expedition');
+			$('.spacer-left-summary').html('Enter and discover more of the scientific expeditions and research travels. Read the stories and study the scientific results.');
+
 			$('#introTitleNL').removeClass('active');
 			$('#introTitleEN').addClass('active');
 			$('#introNL').removeClass('active');
@@ -624,8 +626,8 @@ var InterfaceController = function(ExpeditionController) {
 				value.firstChild.innerHTML = 'Lees meer';
 			});
 
-			// $('#btnBack').text('Terug');
-			$('#btnStartExpedition').text('Start expeditie!');
+			$('.spacer-left-title').html('kies een reis');
+			$('.spacer-left-summary').html('stap in en doe mee met de interessante expedities en ontdek alles over geschiedenis en techniek en leer wat wetenschap zo rijk maakt!');
 
 			$('#introTitleNL').addClass('active');
 			$('#introTitleEN').removeClass('active');
